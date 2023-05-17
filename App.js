5df987b5f3dfb6df272dee95c5af5802692cbda0
OMAR <s11925044@stu.najah.edu>
2023-05-18 00:40:32 +0300

    git command

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-
 const mysql=require('mysql');
+const express=require('express');
+
+
 
 //Connect to database
 
@@ -11,13 +13,40 @@ const db=mysql.createConnection({
     port :'8081'
 });
 
-
 db.connect((err)=>{
     if(err){
         throw err;
     }
    
     console.log('MySql Connected...');
+
+});
+
+
+ const app=express();
+ 
+// git Job List according to id
+
+app.get('/api/job_listing/:id',(req,res)=>{
+    let id=req.params.id;
+    const sql="SELECT * FROM Job_Listing WHERE JobList_ID=";
+    db.query(sql+id,(err,result)=>{
+        if(err)
+        {
+            throw err;
+        }
+        if(!result)
+        {
+            res.status(404).send('This Job_List with given ID was not exist');
+        }
+        res.send(result);
+
+    });
+});
+
+
+app.listen(3000,()=>{
+    console.log('Listening on Port 3000 ...')
 });
 
 
